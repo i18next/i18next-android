fb0b69bf0c9f89349a9abe0a511d9b7531602421
Stan <stan.kocken@gmail.com>
2013-12-20 17:38:47 +0100

    Fix tests

--- src/com/preplay/android/i18next/test/I18NextTest.java
+++ src/com/preplay/android/i18next/test/I18NextTest.java
@@ -7,6 +7,8 @@ import junit.framework.TestCase;
 
 import org.json.JSONException;
 
+import android.annotation.SuppressLint;
+import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Log;
 
 import com.preplay.android.i18next.I18Next;
@@ -45,8 +47,8 @@ public class I18NextTest extends TestCase {
             // "friend_context_female" : "A girlfriend"
             // }
             // }
-            String commonEn = "{\"app\":{\"name\":\"i18next\",\"insert\":\"you are __youAre__\",\"sprintf\":\"%s, %s, %s and %s\",\"sprintf2\":\"%d %f\",\"child\":\"__count__ child\",\"child_plural\":\"__count__ children\",\"child2\":\"a child\",\"child2_plural\":\"some children\",\"area\":\"Area 51\",\"district\":\"District 9 is more fun than $t(app.area)\",\"friend_context\":\"A friend\",\"friend_context_male\":\"A boyfriend\",\"friend_context_female\":\"A girlfriend\"}}";
-            I18Next.getInstance().load("en", "common", commonEn);
+            String content = "{\"app\":{\"name\":\"i18next\",\"insert\":\"you are __youAre__\",\"sprintf\":\"%s, %s, %s and %s\",\"sprintf2\":\"%d %f\",\"child\":\"__count__ child\",\"child_plural\":\"__count__ children\",\"child2\":\"a child\",\"child2_plural\":\"some children\",\"area\":\"Area 51\",\"district\":\"District 9 is more fun than $t(app.area)\",\"friend_context\":\"A friend\",\"friend_context_male\":\"A boyfriend\",\"friend_context_female\":\"A girlfriend\"}}";
+            I18Next.getInstance().load("common_test", content);
         } catch (JSONException e) {
             Log.w(TAG, e);
             fail(e.getMessage());
@@ -57,59 +59,82 @@ public class I18NextTest extends TestCase {
             // "name" : "i18next"
             // }
             // }
-            String commonEn = "{\"app\":{\"name\":\"i18nextspecific\"}}";
-            I18Next.getInstance().load("en", "specific", commonEn);
+            String content = "{\"app\":{\"name\":\"i18nextspecific\"}}";
+            I18Next.getInstance().load("specific", content);
         } catch (JSONException e) {
             Log.w(TAG, e);
             fail(e.getMessage());
         }
-        I18Next.getInstance().getOptions().setDefaultNamespace("common");
+        I18Next.getInstance().getOptions().setDefaultNamespace("common_test");
     }
 
+    @SmallTest
+    public void testShouldAcceptCandidateKey() {
+        String[] listKeyValid = { "test.dot", "test.double.dot", "test.test_underscore", "test.test_double_underscore", "test.test_double_underscore.dot", "test.dot_double_underscore" };
+        String[] listKeyInvalid = { "test", "space not accepted", "double..dot" };
+        for (String key : listKeyValid) {
+            assertTrue("The key '" + key + "' should be accepted as a valid key", I18Next.isI18NextKeyCandidate(key));
+        }
+        for (String key : listKeyInvalid) {
+            assertFalse("The key '" + key + "' should not be accepted as a valid key", I18Next.isI18NextKeyCandidate(key));
+        }
+    }
+
+    @SmallTest
     public void testShouldReturnValueDefault() {
-        assertEquals(I18Next.getInstance().t("notexist"), null);
-        assertEquals(I18Next.getInstance().t("notexist", new Operation.DefaultValue("default")), "default");
+        assertNull(I18Next.getInstance().t("notexist"));
+        assertEquals("default", I18Next.getInstance().t("notexist", new Operation.DefaultValue("default")));
     }
 
+    @SmallTest
     public void testShouldReturnValueWithoutOrDefaultNamespace() {
-        assertEquals(I18Next.getInstance().t("app.name"), "i18next");
+        assertEquals("i18next", I18Next.getInstance().t("app.name"));
     }
 
+    @SmallTest
     public void testShouldReturnValueWithNamespace() {
-        assertEquals(I18Next.getInstance().t("specific:app.name"), "i18nextspecific");
+        assertEquals("i18nextspecific", I18Next.getInstance().t("specific:app.name"));
     }
 
+    @SmallTest
     public void testShouldReturnValueMultiKey() {
-        assertEquals(I18Next.getInstance().t("notexist", "app.name"), "i18next");
+        assertEquals("i18next", I18Next.getInstance().t("notexist", "app.name"));
     }
 
+    @SmallTest
     public void testShouldReturnValueInterpolation() {
-        assertEquals(I18Next.getInstance().t("app.insert", new Operation.Interpolation("youAre", "great")), "you are great");
+        assertEquals("you are great", I18Next.getInstance().t("app.insert", new Operation.Interpolation("youAre", "great")));
     }
 
+    @SmallTest
     public void testShouldReturnValueSprintF() {
-        assertEquals(I18Next.getInstance().t("app.sprintf", new Operation.SPrintF("a", "b", "c", "d")), "a, b, c and d");
+        assertEquals("a, b, c and d", I18Next.getInstance().t("app.sprintf", new Operation.SPrintF("a", "b", "c", "d")));
     }
 
+    @SuppressLint("DefaultLocale")
+    @SmallTest
     public void testShouldReturnValueSprintFWithNumber() {
-        assertEquals(I18Next.getInstance().t("app.sprintf2", new Operation.SPrintF(1, 1.2f)), String.format("%d %f", 1, 1.2f));
-        assertEquals(I18Next.getInstance().t("app.sprintf2", new Operation.SPrintF(1, 1.2d)), String.format("%d %f", 1, 1.2f));
+        assertEquals(String.format("%d %f", 1, 1.2f), I18Next.getInstance().t("app.sprintf2", new Operation.SPrintF(1, 1.2f)));
+        assertEquals(String.format("%d %f", 1, 1.2f), I18Next.getInstance().t("app.sprintf2", new Operation.SPrintF(1, 1.2d)));
     }
 
+    @SmallTest
     public void testShouldReturnValuePlural() {
-        assertEquals(I18Next.getInstance().t("app.child", new Operation.Plural(1)), "1 child");
-        assertEquals(I18Next.getInstance().t("app.child", new Operation.Plural(3)), "3 children");
-        assertEquals(I18Next.getInstance().t("app.child2", new Operation.Plural(1)), "a child");
-        assertEquals(I18Next.getInstance().t("app.child2", new Operation.Plural(3)), "some children");
+        assertEquals("1 child", I18Next.getInstance().t("app.child", new Operation.Plural(1)));
+        assertEquals("3 children", I18Next.getInstance().t("app.child", new Operation.Plural(3)));
+        assertEquals("a child", I18Next.getInstance().t("app.child2", new Operation.Plural(1)));
+        assertEquals("some children", I18Next.getInstance().t("app.child2", new Operation.Plural(3)));
     }
 
+    @SmallTest
     public void testShouldReturnValueNesting() {
-        assertEquals(I18Next.getInstance().t("app.district"), "District 9 is more fun than Area 51");
+        assertEquals("District 9 is more fun than Area 51", I18Next.getInstance().t("app.district"));
     }
 
+    @SmallTest
     public void testShouldReturnValueContext() {
-        assertEquals(I18Next.getInstance().t("app.friend_context", new Operation.Context("")), "A friend");
-        assertEquals(I18Next.getInstance().t("app.friend_context", new Operation.Context("male")), "A boyfriend");
-        assertEquals(I18Next.getInstance().t("app.friend_context", new Operation.Context("female")), "A girlfriend");
+        assertEquals("A friend", I18Next.getInstance().t("app.friend_context", new Operation.Context("")));
+        assertEquals("A boyfriend", I18Next.getInstance().t("app.friend_context", new Operation.Context("male")));
+        assertEquals("A girlfriend", I18Next.getInstance().t("app.friend_context", new Operation.Context("female")));
     }
 }
